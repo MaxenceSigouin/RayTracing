eca567af715de6731871ef9b186e200574080674
Maxence Sigouin <maxence.sigouin@polymtl.ca>
2023-01-18 19:19:39 -0500

    Rolled back to episode 1 code when there was no nullptr exception

--- RayTracingSeries/src/WalnutApp.cpp
+++ RayTracingSeries/src/WalnutApp.cpp
@@ -2,9 +2,8 @@
 #include "Walnut/EntryPoint.h"
 
 #include "Walnut/Image.h"
+#include "Walnut/Random.h"
 #include "Walnut/Timer.h"
-
-#include "Renderer.h"
 
 using namespace Walnut;
 
@@ -27,50 +26,61 @@ public:
 		m_ViewportWidth = ImGui::GetContentRegionAvail().x;
 		m_ViewportHeight = ImGui::GetContentRegionAvail().y;
 
-		auto image = m_Renderer.GetFinalImage();
-		if(image)
-			ImGui::Image(image->GetDescriptorSet(), { (float)image->GetWidth(), (float)image->GetHeight() });
+		if (m_Image)
+			ImGui::Image(m_Image->GetDescriptorSet(), { (float)m_Image->GetWidth(), (float)m_Image->GetHeight() });
 
 		ImGui::End();
 		ImGui::PopStyleVar();
 
 		Render();
-
 	}
 
 	void Render()
 	{
 		Timer timer;
 
-		m_Renderer.OnResize(m_ViewportWidth, m_ViewportHeight);
-		m_Renderer.Render();
+		if (!m_Image || m_ViewportWidth != m_Image->GetWidth() || m_ViewportHeight != m_Image->GetHeight())
+		{
+			m_Image = std::make_shared<Image>(m_ViewportWidth, m_ViewportHeight, ImageFormat::RGBA);
+			delete[] m_ImageData;
+			m_ImageData = new uint32_t[m_ViewportWidth * m_ViewportHeight];
+		}
+
+		for (uint32_t i = 0; i < m_ViewportWidth * m_ViewportHeight; i++)
+		{
+			m_ImageData[i] = Random::UInt();
+			m_ImageData[i] |= 0xff000000;
+		}
+
+		m_Image->SetData(m_ImageData);
 
 		m_LastRenderTime = timer.ElapsedMillis();
 	}
-
 private:
-	Renderer m_Renderer;
+	std::shared_ptr<Image> m_Image;
+	uint32_t* m_ImageData = nullptr;
 	uint32_t m_ViewportWidth = 0, m_ViewportHeight = 0;
-	float m_LastRenderTime = 0;
+
+	float m_LastRenderTime = 0.0f;
 };
 
 Walnut::Application* Walnut::CreateApplication(int argc, char** argv)
 {
 	Walnut::ApplicationSpecification spec;
-	spec.Name = "RayTracing";
+	spec.Name = "Ray Tracing";
 
 	Walnut::Application* app = new Walnut::Application(spec);
 	app->PushLayer<ExampleLayer>();
 	app->SetMenubarCallback([app]()
-	{
-		if (ImGui::BeginMenu("File"))
 		{
-			if (ImGui::MenuItem("Exit"))
+			if (ImGui::BeginMenu("File"))
 			{
-				app->Close();
+				if (ImGui::MenuItem("Exit"))
+				{
+					app->Close();
+				}
+				ImGui::EndMenu();
 			}
-			ImGui::EndMenu();
-		}
-	});
+		});
 	return app;
 }
