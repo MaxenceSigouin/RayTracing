1f64f6cafa33b83f4f59e1c59b94f9fc1df774e5
Maxence Sigouin <maxence.sigouin@polymtl.ca>
2023-01-19 18:26:05 -0500

    Completed Render() method

--- RayTracingSeries/src/Renderer.cpp
+++ RayTracingSeries/src/Renderer.cpp
@@ -1,14 +1,13 @@
 #include "Renderer.h"
 
-void Renderer::Render()
-{
-	// render every pixel
-}
+#include "Walnut/Random.h"
 
 void Renderer::OnResize(uint32_t width, uint32_t height)
 {
 	if (m_Image)
 	{
+		if (m_Image->GetWidth() == width && m_Image->GetHeight() == height) return;
+
 		m_Image->Resize(width, height);
 	}
 	else
@@ -18,4 +17,17 @@ void Renderer::OnResize(uint32_t width, uint32_t height)
 
 	delete[] m_ImageData;
 	m_ImageData = new uint32_t[width * height];
+}
+
+void Renderer::Render()
+{
+
+
+	for (uint32_t i = 0; i < m_Image->GetWidth() * m_Image->GetHeight(); i++)
+	{
+		m_ImageData[i] = Walnut::Random::UInt();
+		m_ImageData[i] |= 0xff000000;
+	}
+
+	m_Image->SetData(m_ImageData);
 }
--- RayTracingSeries/src/Renderer.h
+++ RayTracingSeries/src/Renderer.h
@@ -9,9 +9,8 @@ class Renderer
 public:
 	Renderer() = default;
 
-	void Render();
-
 	void OnResize(uint32_t width, uint32_t height);
+	void Render();
 private:
 	std::shared_ptr < Walnut::Image> m_Image;
 	uint32_t* m_ImageData = nullptr;
