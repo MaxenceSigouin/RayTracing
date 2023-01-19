6e28ef5fe14f40b48ca8dff773d031a21595e11a
Maxence Sigouin <maxence.sigouin@polymtl.ca>
2023-01-19 18:31:32 -0500

    Refactor of OnResize() and Render() done

--- RayTracingSeries/src/Renderer.cpp
+++ RayTracingSeries/src/Renderer.cpp
@@ -4,15 +4,15 @@
 
 void Renderer::OnResize(uint32_t width, uint32_t height)
 {
-	if (m_Image)
+	if (m_FinalImage)
 	{
-		if (m_Image->GetWidth() == width && m_Image->GetHeight() == height) return;
+		if (m_FinalImage->GetWidth() == width && m_FinalImage->GetHeight() == height) return;
 
-		m_Image->Resize(width, height);
+		m_FinalImage->Resize(width, height);
 	}
 	else
 	{
-		m_Image = std::make_shared<Walnut::Image>(width, height, Walnut::ImageFormat::RGBA);
+		m_FinalImage = std::make_shared<Walnut::Image>(width, height, Walnut::ImageFormat::RGBA);
 	}
 
 	delete[] m_ImageData;
@@ -23,11 +23,11 @@ void Renderer::Render()
 {
 
 
-	for (uint32_t i = 0; i < m_Image->GetWidth() * m_Image->GetHeight(); i++)
+	for (uint32_t i = 0; i < m_FinalImage->GetWidth() * m_FinalImage->GetHeight(); i++)
 	{
 		m_ImageData[i] = Walnut::Random::UInt();
 		m_ImageData[i] |= 0xff000000;
 	}
 
-	m_Image->SetData(m_ImageData);
+	m_FinalImage->SetData(m_ImageData);
 }
