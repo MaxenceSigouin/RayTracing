d6f30e47d7b96735f314652a665ee44721837159
Maxence Sigouin <maxence.sigouin@polymtl.ca>
2023-01-22 18:36:32 -0500

    Added interactive camera

--- RayTracingSeries/src/Renderer.cpp
+++ RayTracingSeries/src/Renderer.cpp
@@ -2,6 +2,7 @@
 #include <glm/glm.hpp>
 
 #include "Walnut/Random.h"
+#include "Ray.h"
 
 namespace Utils {
 	static uint32_t ConvertToRGBA(const glm::vec4& color) 
@@ -32,16 +33,19 @@ void Renderer::OnResize(uint32_t width, uint32_t height)
 	m_ImageData = new uint32_t[width * height];
 }
 
-void Renderer::Render()
+void Renderer::Render(const Camera& camera)
 {
+	Ray ray;
+	ray.Origin = camera.GetPosition();
+	const glm::vec3& rayOrigin = camera.GetPosition();
+
 	// y as the outside loop to optimize cache misses (we iterate horizontally instead of vertically)
 	for (uint32_t y = 0; y < m_FinalImage->GetHeight(); y++)
 	{
 		for (uint32_t x = 0; x < m_FinalImage->GetWidth(); x++)
 		{
-			glm::vec2 coord = { (float)x / (float)m_FinalImage->GetWidth(), (float)y / (float)m_FinalImage->GetHeight() };
-			coord = coord * 2.0f - 1.0f;
-			glm::vec4 color = PerPixel(coord);
+			ray.Direction = camera.GetRayDirections()[x + y * m_FinalImage->GetWidth()];
+			glm::vec4 color = TraceRay(ray);
 			color = glm::clamp(color, glm::vec4(0.0f), glm::vec4(1.0f));
 			m_ImageData[x + y * m_FinalImage->GetWidth()] = Utils::ConvertToRGBA(color);
 		}
@@ -50,10 +54,8 @@ void Renderer::Render()
 	m_FinalImage->SetData(m_ImageData);
 }
 
-glm::vec4 Renderer::PerPixel(glm::vec2 coord) 
+glm::vec4 Renderer::TraceRay(const Ray& ray) 
 {
-	glm::vec3 rayOrigin(0.0f, 0.0f, 1.0f);
-	glm::vec3 rayDirection(coord.x, coord.y, -1.0f);
 	float radius = 0.5f;
 
 	// (bx^2 + by^2)t^2 + 2(axbx + ayby)t + (ax^2 + ay^2 - r^2) = 0, solving for t
@@ -63,9 +65,9 @@ glm::vec4 Renderer::PerPixel(glm::vec2 coord)
 	// r = radius
 	// t = hit distance
 
-	float a = glm::dot(rayDirection, rayDirection);
-	float b = 2.0f * glm::dot(rayOrigin, rayDirection);
-	float c = glm::dot(rayOrigin, rayOrigin) - radius * radius;
+	float a = glm::dot(ray.Direction, ray.Direction);
+	float b = 2.0f * glm::dot(ray.Origin, ray.Direction);
+	float c = glm::dot(ray.Origin, ray.Origin) - radius * radius;
 
 	// Quadratic formula discriminant:
 	// b^2 - 4ac
@@ -80,7 +82,7 @@ glm::vec4 Renderer::PerPixel(glm::vec2 coord)
 	float t1 = (-b - glm::sqrt(discriminant)) / (2.0f * a);
 
 	// T1 is always gonna be closest so no need to calculate for t0 too
-	glm::vec3 hitPoint = rayOrigin + rayDirection * t1;
+	glm::vec3 hitPoint = ray.Origin + ray.Direction * t1;
 	glm::vec3 normal = glm::normalize(hitPoint);
 
 	glm::vec3 lightDir = glm::normalize(glm::vec3( - 1, -1, -1));
