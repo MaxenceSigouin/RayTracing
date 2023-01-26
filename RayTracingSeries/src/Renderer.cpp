5123207df40fd29dcea87381dfc33cda1affb26d
Maxence Sigouin <maxence.sigouin@polymtl.ca>
2023-01-25 21:34:50 -0500

    Added bounces to rays so objects can reflect off of each other

--- RayTracingSeries/src/Renderer.cpp
+++ RayTracingSeries/src/Renderer.cpp
@@ -84,7 +84,7 @@ Renderer::HitPayload Renderer::TraceRay(const Ray& ray)
 		// (-b +- sqrt(discriminant)) / 2a
 
 		float t = (-b - glm::sqrt(discriminant)) / (2.0f * a);
-		if (t < hitDistance)
+		if (t > 0.0f && t < hitDistance)
 		{
 			hitDistance = t;
 			closestSphere = int(i);
@@ -102,17 +102,36 @@ glm::vec4 Renderer::PerPixel(uint32_t x, uint32_t y)
 	ray.Origin = m_ActiveCamera->GetPosition();
 	ray.Direction = m_ActiveCamera->GetRayDirections()[x + y * m_FinalImage->GetWidth()];
 
-	Renderer::HitPayload payload = TraceRay(ray);
-	if (payload.hitDistance < 0.0f) return glm::vec4(0.0f, 0.0f, 0.0f, 1.0f);
+	glm::vec3 color(0.0f, 0.0f, 0.0f);
+	float multiplier = 1.0f;
 
-	glm::vec3 lightDir = glm::normalize(glm::vec3(-1, -1, -1));
-	float lightIntensity = glm::max(glm::dot(payload.WorldNormal, -lightDir), 0.0f); // == cos(angle)
+	int bounces = 2;
+	for (int i = 0; i < bounces; i++)
+	{
+		Renderer::HitPayload payload = TraceRay(ray);
+		if (payload.hitDistance < 0.0f)
+		{
+			glm::vec3 skyColor = glm::vec3(0.0f, 0.0f, 0.0f);
+			color += skyColor * multiplier; 
+			break;
+		}
 
-	const Sphere& sphere = m_ActiveScene->Spheres[payload.ObjectIndex];
+		glm::vec3 lightDir = glm::normalize(glm::vec3(-1, -1, -1));
+		float lightIntensity = glm::max(glm::dot(payload.WorldNormal, -lightDir), 0.0f); // == cos(angle)
 
-	glm::vec3 sphereColor = sphere.Albedo;
-	sphereColor *= lightIntensity;
-	return glm::vec4(sphereColor, 1.0f);
+		const Sphere& sphere = m_ActiveScene->Spheres[payload.ObjectIndex];
+
+		glm::vec3 sphereColor = sphere.Albedo;
+		sphereColor *= lightIntensity;
+		color += sphereColor * multiplier;
+
+		multiplier *= 0.7f;
+
+		ray.Origin = payload.WorldPosition + payload.WorldNormal * 0.0001f;
+		ray.Direction = glm::reflect(ray.Direction, payload.WorldNormal);
+	}
+
+	return glm::vec4(color, 1.0f);
 }
 
 Renderer::HitPayload Renderer::ClosestHit(const Ray& ray, float hitDistance, int objectIndex)
